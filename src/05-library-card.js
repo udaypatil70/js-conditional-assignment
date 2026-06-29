b5480d0ecaf7854989d8b5de587da687a9bf67b3
Uday Patil <udayp9020@gmail.com>
2026-06-29 19:10:38 +0530

    feat: Impliment library-card eligibility

--- src/05-library-card.js
+++ src/05-library-card.js
@@ -34,4 +34,28 @@
  */
 export function canBorrowBook(memberAge, hasValidCard, overdueBooks) {
   // Your code here
+  if(memberAge < 6){
+    return {
+      allowed: false,
+      message: "Too young - must be at least 6 years old"
+    }
+  }
+  if(!hasValidCard){
+      return{
+        allowed: false,
+        message: "Invalid library card - please renew at the front desk"
+      }
+  }
+  if(overdueBooks){
+      return {
+        allowed: false,
+        message: `Please return your ${overdueBooks} overdue book(s) first`
+      }
+    }
+  
+  return {
+    allowed: true,
+    message: "You may borrow up to 3 books"
+  }
 }
+
