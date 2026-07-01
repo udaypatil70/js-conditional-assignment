76a2d1cde5a2fee44a9b64dfc0bae8891e49867a
Uday Patil <udayp9020@gmail.com>
2026-07-01 17:11:34 +0530

    feat: Implimenting Coffee-Shop Item Pricing

--- src/07-coffee-shop.js
+++ src/07-coffee-shop.js
@@ -32,4 +32,44 @@
  */
 export function calculateCoffeePrice(size, type, extras = {}) {
   // Your code here
+  if (size !== "small" && size !== "medium" && size !== "large") {
+    return -1;
+  }
+  if (
+    type !== "regular" &&
+    type !== "latte" &&
+    type !== "cappuccino" &&
+    type !== "mocha"
+  ) {
+    return -1;
+  }
+  let price;
+  if (size === "small") {
+    price = 3.0;
+  }
+  if (size === "medium") {
+    price = 4.0;
+  }
+  if (size === "large") {
+    price = 5.0;
+  }
+  if (type === "regular") {
+    price += 0.0;
+  }
+  if (type === "latte") {
+    price += 1.0;
+  }
+  if (type === "cappuccino") {
+    price += 1.5;
+  }
+  if (type === "mocha") {
+    price += 2.0;
+  }
+  if (extras.whippedCream) {
+    price += 0.5;
+  }
+  if (extras.extraShot) {
+    price += 0.75;
+  }
+  return parseFloat(price.toFixed(2));
 }
