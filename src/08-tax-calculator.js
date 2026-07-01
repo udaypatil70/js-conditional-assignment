52e8d564ebcfb32077f2ae61d0bb16815f493439
Uday Patil <udayp9020@gmail.com>
2026-07-01 17:03:28 +0530

    feat: Impliment tax calculator Logic

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,4 +27,17 @@
  */
 export function calculateTax(income) {
   // Your code here
+  if (income <= 0) {
+    return 0;
+  }
+ if(income <= 10000) {
+    return 0;
+  } 
+  if (income <= 30000) {
+    return (income - 10000) * 0.1;
+  }
+  if (income <= 70000) {
+    return 2000 + (income - 30000) * 0.2;
+  }
+  return 10000 + (income - 70000) * 0.3;
 }
